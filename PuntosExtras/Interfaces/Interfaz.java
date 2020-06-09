7bef7cb851c22c9cadc24d1ade022fb07bd5d459
Edag09 <3006840880101@ingenieria.usac.edu.gt>
2020-06-08 20:37:47 -0600

    ADD: anexo de puntps extras

---
+++ PuntosExtras/Interfaces/Interfaz.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public class Interfaz {
+    
+    public static void main(String[] args) {
+        Principal p = new  Principal();
+        p.setVisible(true);
+    }
+    
+}
